93895520f9fe47213ad9edbfe27f4211ae33403a
Mo3iBest <mo3iiibest77@gmail.com>
2026-08-19 00:33:05 +0000

    fix: revert streamSecurity, add cipherSuites/finalMask/fingerPrint defaults

--- V2rayNG/app/src/main/java/com/v2ray/ang/ui/server/ServerActivity.kt
+++ V2rayNG/app/src/main/java/com/v2ray/ang/ui/server/ServerActivity.kt
@@ -263,7 +263,7 @@ fun ServerScreen(
     var allowInsecure by rememberSaveable { mutableStateOf(initialConfig.insecure == true) }
     var fingerPrint by rememberSaveable { mutableStateOf(initialConfig.fingerPrint?.takeIf { it.isNotBlank() } ?: "unsafe") }
     var alpn by rememberSaveable { mutableStateOf(initialConfig.alpn ?: "") }
-    var cipherSuites by rememberSaveable { mutableStateOf(initialConfig.cipherSuites ?: "") }
+    var cipherSuites by rememberSaveable { mutableStateOf(initialConfig.cipherSuites?.takeIf { it.isNotBlank() } ?: "TLS_AES_256_GCM_SHA384:TLS_CHACHA20_POLY1305_SHA256:TLS_AES_128_GCM_SHA256:TLS_ECDHE_ECDSA_WITH_AES_256_GCM_SHA384:TLS_ECDHE_RSA_WITH_AES_256_GCM_SHA384:TLS_ECDHE_ECDSA_WITH_AES_128_GCM_SHA256:TLS_ECDHE_RSA_WITH_AES_128_GCM_SHA256:TLS_ECDHE_ECDSA_WITH_CHACHA20_POLY1305_SHA256:TLS_ECDHE_RSA_WITH_CHACHA20_POLY1305_SHA256:TLS_ECDHE_ECDSA_WITH_AES_256_CBC_SHA:TLS_ECDHE_RSA_WITH_AES_256_CBC_SHA:TLS_ECDHE_ECDSA_WITH_AES_128_CBC_SHA256:TLS_ECDHE_RSA_WITH_AES_128_CBC_SHA256") }
     var publicKeyReality by rememberSaveable { mutableStateOf(initialConfig.publicKey ?: "") }
     var shortId by rememberSaveable { mutableStateOf(initialConfig.shortId ?: "") }
     var spiderX by rememberSaveable { mutableStateOf(initialConfig.spiderX ?: "") }
